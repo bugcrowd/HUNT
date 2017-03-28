01b70b112c3a08f1506ba3395b5df7d2d36f6923
JP Villanueva <jp.villanueva@bugcrowd.com>
2017-03-28 10:46:34 -0700

    Add two panes into JSplitPane

--- bugcatcher.py
+++ bugcatcher.py
@@ -1,18 +1,73 @@
 import json
 from burp import IBurpExtender
-from java.io import PrintWriter
+from burp import ITab
+from java.awt import Dimension
+from javax import swing
+from javax.swing import BoxLayout
+from javax.swing import JLabel
+from javax.swing import JPanel
+from javax.swing import JSplitPane
+from javax.swing import SwingConstants
 
-class BurpExtender(IBurpExtender):
+class BurpExtender(IBurpExtender, ITab):
+    EXTENSION_NAME = "Bug Catcher"
+
     def registerExtenderCallbacks(self, callbacks):
-        callbacks.setExtensionName("Bug Catcher")
-
-        stdout = PrintWriter(callbacks.getStdout(), True)
-
-        with open('issues.json') as data_file:
-            data = json.load(data_file)
-
-            for p in data["issues"]:
-                stdout.println(p)
+        self.checklist()
+        self._helpers = callbacks.getHelpers()
+        self._callbacks = callbacks
+        self._callbacks.setExtensionName(self.EXTENSION_NAME)
+        self._callbacks.addSuiteTab(self)
 
         return
 
+    def checklist(self):
+        self._jPanel = JPanel()
+        self._jPanel.setLayout(swing.BoxLayout(self._jPanel, swing.BoxLayout.X_AXIS))
+
+        # Create panes
+        methodology_pane = self.methodology()
+        subtabs_pane = self.subtabs()
+
+        self._jSplitPane = JSplitPane()
+        self._jSplitPane.setLeftComponent(methodology_pane)
+        self._jSplitPane.setRightComponent(subtabs_pane)
+        self._jPanel.add(self._jSplitPane)
+
+        return
+
+    def methodology(self):
+        box_vertical = swing.Box.createVerticalBox()
+        box_horizontal = swing.Box.createHorizontalBox()
+        box_horizontal.add(swing.JLabel("Checklist", SwingConstants.RIGHT))
+        box_vertical.add(box_horizontal)
+        box_horizontal = swing.Box.createHorizontalBox()
+        self._results_textarea = swing.JTextArea()
+        results_output = swing.JScrollPane(self._results_textarea)
+        box_horizontal.add(results_output)
+        box_vertical.add(box_horizontal)
+
+        #self._jPanel.add(box_vertical)
+
+        return box_vertical
+
+    def subtabs(self):
+        box_vertical2 = swing.Box.createVerticalBox()
+        box_horizontal2 = swing.Box.createHorizontalBox()
+        box_horizontal2.add(swing.JLabel("Tabs"))
+        box_vertical2.add(box_horizontal2)
+        box_horizontal2 = swing.Box.createHorizontalBox()
+        self._results_textarea = swing.JTextArea()
+        results_output2 = swing.JScrollPane(self._results_textarea)
+        box_horizontal2.add(results_output2)
+        box_vertical2.add(box_horizontal2)
+
+        #self._jPanel.add(box_vertical2)
+
+        return box_vertical2
+
+    def getTabCaption(self):
+        return self.EXTENSION_NAME
+
+    def getUiComponent(self):
+        return self._jPanel
