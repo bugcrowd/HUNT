768f658d7e259be652ed2d1c4d2051b6708f4a35
JP Villanueva <jp.villanueva@bugcrowd.com>
2017-04-10 14:34:10 -0700

    Add createMenuItems() function to create a context menu item into the HTTP history of Burp to send requests/responses to the plugin

--- bugcatcher.py
+++ bugcatcher.py
@@ -1,8 +1,13 @@
 import json
 from burp import IBurpExtender
+from burp import IContextMenuFactory
+from burp import IContextMenuInvocation
 from burp import ITab
 from javax import swing
 from javax.swing import JCheckBox
+from javax.swing import JMenu
+from javax.swing import JMenuBar
+from javax.swing import JMenuItem
 from javax.swing import JLabel
 from javax.swing import JPanel
 from javax.swing import JSplitPane
@@ -17,7 +22,7 @@ from javax.swing.tree import TreeSelectionModel
 
 # TODO: Refactor to move functions into their own classes based on
 # functionality
-class BurpExtender(IBurpExtender, ITab):
+class BurpExtender(IBurpExtender, IContextMenuFactory, ITab):
     EXTENSION_NAME = "Bug Catcher"
 
     def registerExtenderCallbacks(self, callbacks):
@@ -26,10 +31,36 @@ class BurpExtender(IBurpExtender, ITab):
         self.pane = self.create_pane()
         self.helpers = callbacks.getHelpers()
         self.callbacks.setExtensionName(self.EXTENSION_NAME)
+        self.callbacks.registerContextMenuFactory(self)
         self.callbacks.addSuiteTab(self)
 
         return
 
+    def createMenuItems(self, invocation):
+        data = self.data
+        functionality = data["functionality"]
+
+        # Create the menu item for the Burp context menu
+        bugcatcher_menu = JMenu("Send to Bug Catcher")
+
+        # TODO: Sort the functionality by name and by vuln class
+        for functionality_name in functionality:
+            vulns = functionality[functionality_name]["vulns"]
+            menu_vuln = JMenu(functionality_name)
+
+            for vuln_name in vulns:
+                item_vuln = JMenuItem(vuln_name)
+                menu_vuln.add(vuln_name)
+
+            bugcatcher_menu.add(menu_vuln)
+
+
+        burp_menu = []
+        burp_menu.append(bugcatcher_menu)
+
+        return burp_menu
+
+    # TODO: Move to Data class
     def get_data(self):
         with open("checklist.json") as data_file:
             data = json.load(data_file)
@@ -37,6 +68,10 @@ class BurpExtender(IBurpExtender, ITab):
 
         return checklist
 
+    # TODO: Move to View class
+    # TODO: Figure out how to use JCheckboxTree instead of a simple JTree
+    # TODO: Change icons to Bugcrowd logo for brief, VRT logo for vulns,
+    #       bullseye for Targets, etc
     # Creates a tree event listener to dynamically render each vuln class
     # as its own pane
     def create_pane(self):
@@ -60,6 +95,7 @@ class BurpExtender(IBurpExtender, ITab):
 
         return pane
 
+    # TODO: Move to View class
     # TODO: Use Bugcrowd API to grab the Program Brief and Targets
     # Creates the tree dynamically using the JSON file
     def create_checklist_tree(self):
@@ -70,7 +106,7 @@ class BurpExtender(IBurpExtender, ITab):
         root.add(DefaultMutableTreeNode("Program Brief"))
         root.add(DefaultMutableTreeNode("Targets"))
 
-        # TODO: Sort the functionality by name
+        # TODO: Sort the functionality by name and by vuln class
         for functionality_name in functionality:
             vulns = functionality[functionality_name]["vulns"]
             node = DefaultMutableTreeNode(functionality_name)
@@ -128,7 +164,7 @@ class TSL(TreeSelectionListener):
                 name = node.toString()
                 functionality_textarea = JTextArea()
                 functionality_textarea.setLineWrap(True)
-                functionality_textarea.setText(name)
+                functionality_textarea.setText("Make a description for: " + name)
 
                 pane.setRightComponent(functionality_textarea)
         else:
@@ -150,6 +186,10 @@ class TSL(TreeSelectionListener):
         description_textarea.setText(description_text)
         description_panel = JScrollPane(description_textarea)
 
+        # Renders the bugs tab
+        bugs_textarea = JScrollPane()
+        bugs_panel = JScrollPane(bugs_textarea)
+
         # Renders the resources tab
         resource_textarea = JTextArea()
         resource_textarea.setLineWrap(True)
@@ -159,6 +199,7 @@ class TSL(TreeSelectionListener):
 
         tabbed_pane = JTabbedPane()
         tabbed_pane.add("Description", description_panel)
+        tabbed_pane.add("Bugs", bugs_panel)
         tabbed_pane.add("Resources", resources_panel)
 
         return tabbed_pane
