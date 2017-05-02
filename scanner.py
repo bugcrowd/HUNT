df3b83e226c9e38d973ae6febf4d6bea8f7e4a7e
JP Villanueva <jp.villanueva@bugcrowd.com>
2017-05-02 11:18:37 -0700

    Initial commit for passive scanner

--- scanner.py
+++ scanner.py
@@ -8,8 +8,7 @@ class BurpExtender(IBurpExtender, IScannerCheck):
     EXTENSION_NAME = "Bug Catcher Scanner"
 
     def __init__(self):
-        issues = Issues()
-        self.issues = issues.get_issues()
+        self.issues = Issues()
 
     def registerExtenderCallbacks(self, callbacks):
         self.callbacks = callbacks
@@ -18,6 +17,8 @@ class BurpExtender(IBurpExtender, IScannerCheck):
         self.callbacks.registerScannerCheck(self)
 
     def doPassiveScan(self, request_response):
+        current_issues = []
+
         raw_request = request_response.getRequest()
         raw_response = request_response.getResponse()
         request = self.helpers.analyzeRequest(raw_request)
@@ -26,48 +27,79 @@ class BurpExtender(IBurpExtender, IScannerCheck):
         parameters = request.getParameters()
         vuln_parameters = self.check_parameters(parameters)
 
-        is_not_empty = len(vuln_parameters) != 0
+        is_not_empty = len(vuln_parameters) > 0
 
         if is_not_empty:
-            print vuln_parameters
-            self.create_scanner_issues(vuln_parameters, request_response)
+            current_issues = self.create_scanner_issues(vuln_parameters, request_response)
+            return self.issues.get_scanner_issues()
         else:
             print "No vuln parameters"
+            return []
 
-        return
+    def consolidateDuplicateIssues(self, existing, new):
+        raw_request = existing.getHttpMessages()[0]
+        parameters = self.helpers.analyzeRequest(raw_request).getParameters()
+        issues = self.issues.get_scanner_issues()
+        url = existing.getHttpMessages()[0].getUrl().toString()
+        check_parameters = []
+
+        for parameter in parameters:
+            # Checks if value is 0 for params in GET query string or 1 for params in POST body
+            # https://portswigger.net/burp/extender/api/constant-values.html#burp.IParameter
+            is_parameter = parameter.getType() == 0 or parameter.getType() == 1
+            is_same_url = existing.getUrl() == new.getUrl()
+
+            if is_same_url and is_parameter:
+                return -1
+            else:
+                return 0
 
     def getTabCaption(self):
         return self.EXTENSION_NAME
 
     def check_parameters(self, parameters):
         vuln_parameters = []
+        issues = self.issues.get_issues()
 
         for parameter in parameters:
-            # Handles double URL encoding just in case
-            parameter_decoded = unquote(unquote(parameter.getName()))
+            # Checks if value is 0 for params in GET query string or 1 for params in POST body
+            is_parameter = parameter.getType() == 0 or parameter.getType() == 1
+
+            if is_parameter:
+                # Handles double URL encoding just in case
+                # TODO: Change to use Burp method for URL decode
+                parameter_decoded = unquote(unquote(parameter.getName()))
+            else:
+                continue
 
             # Check to see if the current parameter is a potentially vuln parameter
-            is_vuln_found = parameter_decoded in self.issues
+            for issue in issues:
+                vuln_parameter = str(issue.keys()[0])
+                is_vuln_found = parameter_decoded == vuln_parameter
 
-            if is_vuln_found:
-                vuln_parameters.append(parameter_decoded)
+                if is_vuln_found:
+                    vuln_parameters.append(issue)
 
-        return set(vuln_parameters)
+        return vuln_parameters
 
     def create_scanner_issues(self, vuln_parameters, request_response):
         for vuln_parameter in vuln_parameters:
-            scanner_issue = ScannerIssue(self.helpers, vuln_parameter, vuln_parameters, request_response)
+            issues = self.issues.get_json()
+            parameter = str(vuln_parameter.keys()[0])
+            vuln_name = vuln_parameter.get(parameter)
 
-            is_unique = scanner_issue in self.get_scanner_issues()
+            url = self.helpers.analyzeRequest(request_response).getUrl()
+            http_service = request_response.getHttpService()
+            http_messages = [self.callbacks.applyMarkers(request_response, None, None)]
+            detail = issues["issues"][vuln_name]["detail"]
+            severity = "Medium"
 
-            if is_unique:
-                self.issue.set_scanner_issue(scanner_issue)
+            scanner_issue = ScannerIssue(url, parameter, http_service, http_messages, vuln_name, detail, severity)
 
-        return
+            self.issues.set_scanner_issues(scanner_issue)
 
 class Issues:
-    issues = set()
-    scanner_issues = set()
+    scanner_issues = []
     shared_state = {}
 
     def __init__(self):
@@ -83,37 +115,69 @@ class Issues:
         return self.json
 
     def set_issues(self):
+        self.issues = []
         issues = self.json["issues"]
 
         for vuln_name in issues:
             parameters = issues[vuln_name]["params"]
 
             for parameter in parameters:
-                self.issues.add(parameter + "." + vuln_name)
-
-        return self.issues
+                issue = {}
+                issue[parameter] = vuln_name
+                self.issues.append(issue)
 
     def get_issues(self):
         return self.issues
 
     def set_scanner_issues(self, scanner_issue):
-        scanner_issues.add(scanner_issue)
-
-        return
+        self.scanner_issues.append(scanner_issue)
 
     def get_scanner_issues(self):
-        return scanner_issues
+        return self.scanner_issues
 
 class ScannerIssue(IScanIssue):
-    def __init__(self, helpers, vuln_parameter, vuln_parameters, request_response):
-        raw_request = request_response.getRequest()
-        raw_response = request_response.getResponse()
+    def __init__(self, url, parameter, http_service, http_messages, vuln_name, detail, severity):
+        self.this_url = url
+        self.http_service = http_service
+        self.http_messages = http_messages
+        self.detail = detail.replace("$params", parameter)
+        self.this_severity = severity
+        self.issue_background = "Background"
+        self.vuln_name = vuln_name
+        self.parameter = parameter
 
-        self.request = helpers.analyzeRequest(raw_request)
-        self.response = helpers.analyzeRequest(raw_response)
-        self.http_service = request_response.getHttpService()
-        self.url = self.request.getUrl()
-        self.detail = vuln_parameters[vuln_parameter]["detail"]
-        self.severity = vuln_parameters[vuln_parameter]["severity"]
-        self.parameter = vuln_parameter
+    def getParameter(self):
+        return self.parameter
 
+    def getUrl(self):
+        return self.this_url
+
+    def getIssueName(self):
+        return self.vuln_name
+
+    def getIssueType(self):
+        return 0
+
+    def getSeverity(self):
+        return self.this_severity
+
+    def getConfidence(self):
+        return "Certain"
+
+    def getIssueBackground(self):
+        return self.issue_background
+
+    def getRemediationBackground(self):
+        pass
+
+    def getIssueDetail(self):
+        return self.detail
+
+    def getRemediationDetail(self):
+        return None
+
+    def getHttpMessages(self):
+        return self.http_messages
+
+    def getHttpService(self):
+        return self.http_service
