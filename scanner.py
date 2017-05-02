7159fb23f28a888c073543526d6dda1019aa4769
JP Villanueva <jp.villanueva@bugcrowd.com>
2017-05-02 14:56:02 -0700

    Add check_duplicate_issue function to check if the newest issue is already in the Issues array of ScannerIssues

--- scanner.py
+++ scanner.py
@@ -1,5 +1,4 @@
 import json
-from urllib import unquote
 from burp import IBurpExtender
 from burp import IScannerCheck
 from burp import IScanIssue
@@ -17,7 +16,7 @@ class BurpExtender(IBurpExtender, IScannerCheck):
         self.callbacks.registerScannerCheck(self)
 
     def doPassiveScan(self, request_response):
-        current_issues = []
+        current_issues = self.issues.get_scanner_issues()
 
         raw_request = request_response.getRequest()
         raw_response = request_response.getResponse()
@@ -30,29 +29,16 @@ class BurpExtender(IBurpExtender, IScannerCheck):
         is_not_empty = len(vuln_parameters) > 0
 
         if is_not_empty:
-            current_issues = self.create_scanner_issues(vuln_parameters, request_response)
-            return self.issues.get_scanner_issues()
-        else:
-            print "No vuln parameters"
-            return []
+            self.create_scanner_issues(vuln_parameters, request_response)
 
-    def consolidateDuplicateIssues(self, existing, new):
-        raw_request = existing.getHttpMessages()[0]
-        parameters = self.helpers.analyzeRequest(raw_request).getParameters()
-        issues = self.issues.get_scanner_issues()
-        url = existing.getHttpMessages()[0].getUrl().toString()
-        check_parameters = []
+        # Test code
+        for issue in current_issues:
+            print issue.getUrl()
+            print issue.getParameter()
+            print
 
-        for parameter in parameters:
-            # Checks if value is 0 for params in GET query string or 1 for params in POST body
-            # https://portswigger.net/burp/extender/api/constant-values.html#burp.IParameter
-            is_parameter = parameter.getType() == 0 or parameter.getType() == 1
-            is_same_url = existing.getUrl() == new.getUrl()
-
-            if is_same_url and is_parameter:
-                return -1
-            else:
-                return 0
+        # Do not show any Bugcrowd found issues in the Scanner window
+        return None
 
     def getTabCaption(self):
         return self.EXTENSION_NAME
@@ -62,13 +48,14 @@ class BurpExtender(IBurpExtender, IScannerCheck):
         issues = self.issues.get_issues()
 
         for parameter in parameters:
-            # Checks if value is 0 for params in GET query string or 1 for params in POST body
-            is_parameter = parameter.getType() == 0 or parameter.getType() == 1
+            # Make sure that the parameter is not from the cookies
+            # https://portswigger.net/burp/extender/api/constant-values.html#burp.IParameter
+            is_not_cookie = parameter.getType() != 2
 
-            if is_parameter:
-                # Handles double URL encoding just in case
-                # TODO: Change to use Burp method for URL decode
-                parameter_decoded = unquote(unquote(parameter.getName()))
+            if is_not_cookie:
+                # Handle double URL encoding just in case
+                parameter_decoded = self.helpers.urlDecode(parameter.getName())
+                parameter_decoded = self.helpers.urlDecode(parameter_decoded)
             else:
                 continue
 
@@ -94,9 +81,25 @@ class BurpExtender(IBurpExtender, IScannerCheck):
             detail = issues["issues"][vuln_name]["detail"]
             severity = "Medium"
 
-            scanner_issue = ScannerIssue(url, parameter, http_service, http_messages, vuln_name, detail, severity)
+            is_not_dupe = self.check_duplicate_issue(url, parameter, vuln_name)
 
-            self.issues.set_scanner_issues(scanner_issue)
+            if is_not_dupe:
+                scanner_issue = ScannerIssue(url, parameter, http_service, http_messages, vuln_name, detail, severity)
+                self.issues.set_scanner_issues(scanner_issue)
+
+    def check_duplicate_issue(self, url, parameter, vuln_name):
+        issues = self.issues.get_scanner_issues()
+
+        for issue in issues:
+            is_same_url = url == issue.getUrl()
+            is_same_parameter = parameter == issue.getParameter()
+            is_same_vuln_name = vuln_name == issue.getIssueName()
+            is_dupe = is_same_url and is_same_parameter and is_same_vuln_name
+
+            if is_dupe:
+                return False
+
+        return True
 
 class Issues:
     scanner_issues = []
@@ -140,9 +143,9 @@ class ScannerIssue(IScanIssue):
         self.this_url = url
         self.http_service = http_service
         self.http_messages = http_messages
-        self.detail = detail.replace("$params", parameter)
+        self.detail = detail.replace("$param$", parameter)
         self.this_severity = severity
-        self.issue_background = "Background"
+        self.issue_background = "Bugcrowd"
         self.vuln_name = vuln_name
         self.parameter = parameter
 
