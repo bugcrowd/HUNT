3f80b24c55d1dccc8f16f0e25ed7f8f5cc1a1190
JP Villanueva <jp.villanueva@bugcrowd.com>
2017-12-03 15:35:03 -0800

    Remove ScannerTableListener class from the main HUNT scanner file

--- Burp/hunt_scanner.py
+++ Burp/hunt_scanner.py
@@ -48,7 +48,6 @@ from javax.swing import BoxLayout
 from javax.swing import SwingUtilities
 from javax.swing import SwingConstants
 from javax.swing.event import ListSelectionListener
-from javax.swing.event import TableModelListener
 from javax.swing.event import TreeSelectionListener
 from javax.swing.tree import DefaultMutableTreeNode
 from javax.swing.tree import TreeSelectionModel
@@ -520,26 +519,6 @@ class SettingsAction(ActionListener):
                 json.dump(data, out_file, indent=2, sort_keys=True)
         except SaveIssuesFileError as e:
             print e
-
-class ScannerTableListener(TableModelListener):
-    def __init__(self, view, scanner_table, issue_name, issue_param):
-        self.view = view
-        self.scanner_table = scanner_table
-        self.issue_name = issue_name
-        self.issue_param = issue_param
-
-    def tableChanged(self, e):
-        row = e.getFirstRow()
-        col = e.getColumn()
-        is_checked = self.scanner_table.getValueAt(row, col)
-        is_changed = e.getType() == e.UPDATE
-
-        if is_changed:
-            self.view.get_issues_object().change_total_count(self.issue_name, is_checked)
-            self.view.get_issues_object().change_issues_count(self.issue_name, self.issue_param, is_checked)
-            issue_count = self.view.get_issues_object().get_issues_count(self.issue_name, self.issue_param)
-            total_count = self.view.get_issues_object().get_total_count(self.issue_name)
-            self.view.set_scanner_count(self.issue_name, self.issue_param, issue_count, total_count)
 
 class TSL(TreeSelectionListener):
     def __init__(self, view):
