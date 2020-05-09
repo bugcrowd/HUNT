cf9ee1b4dc79af4c582b22e378b455f1bd6014fb
cak <cak@fastmail.com>
2020-05-09 06:27:31 -0400

    Add Hunt tab to extender

--- Remix/BurpRemix/src/main/kotlin/BurpExtender.kt
+++ Remix/BurpRemix/src/main/kotlin/BurpExtender.kt
@@ -2,11 +2,13 @@ package burp
 
 class BurpExtender : IBurpExtender {
     override fun registerExtenderCallbacks(callbacks: IBurpExtenderCallbacks) {
-        callbacks.registerHttpListener(HuntListener(callbacks))
+        val tab = HuntTab(callbacks)
+        callbacks.registerHttpListener(HuntListener(callbacks, tab))
         callbacks.stdout.write("HUNT Remix - v0.0.1".toByteArray())
         callbacks.stdout.write("\nOriginally by: JP Villanueva, Jason Haddix and team at Bugcrowd".toByteArray())
         callbacks.stdout.write("\nRepo: https://github.com/bugcrowd/HUNT".toByteArray())
         callbacks.stdout.write("\nRemixed by: Caleb Kinney (derail.io)".toByteArray())
         callbacks.setExtensionName("HUNT Remix")
+        callbacks.addSuiteTab(tab)
     }
 }
