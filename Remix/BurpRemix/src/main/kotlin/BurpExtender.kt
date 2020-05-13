640a19a31d54ab56aa420272edadb87b4272b1cd
cak <cak@fastmail.com>
2020-05-12 21:04:18 -0400

    v0.0.2

--- Remix/BurpRemix/src/main/kotlin/BurpExtender.kt
+++ Remix/BurpRemix/src/main/kotlin/BurpExtender.kt
@@ -4,7 +4,7 @@ class BurpExtender : IBurpExtender {
     override fun registerExtenderCallbacks(callbacks: IBurpExtenderCallbacks) {
         val tab = HuntTab(callbacks)
         callbacks.registerHttpListener(HuntListener(callbacks, tab))
-        callbacks.stdout.write("HUNT Remix - v0.0.1".toByteArray())
+        callbacks.stdout.write("HUNT Remix - v0.0.2".toByteArray())
         callbacks.stdout.write("\nOriginally by: JP Villanueva, Jason Haddix and team at Bugcrowd".toByteArray())
         callbacks.stdout.write("\nRepo: https://github.com/bugcrowd/HUNT".toByteArray())
         callbacks.stdout.write("\nRemixed by: Caleb Kinney (derail.io)".toByteArray())
