82c954e4c8a9a9611e66dfc9ec89875d8f353935
cak <cak@fastmail.com>
2020-06-15 03:19:16 -0400

    Add Kotlin Coroutines dependency

--- Remix/BurpRemix/build.gradle.kts
+++ Remix/BurpRemix/build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("net.portswigger.burp.extender:burp-extender-api:2.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.3.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
 }
 
 tasks {
