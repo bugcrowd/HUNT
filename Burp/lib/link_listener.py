b4f287d7e6e74e2341406d16d2eb7f43f2ef27d3
Christian Clauss <cclauss@me.com>
2020-06-23 10:24:58 +0200

    noqa: F821 Jython defines Desktop

--- Burp/lib/link_listener.py
+++ Burp/lib/link_listener.py
@@ -3,6 +3,6 @@ from javax.swing.event import HyperlinkListener
 class LinkListener(HyperlinkListener):
     def hyperlinkUpdate(self, hle):
         if hle.EventType.ACTIVATED == hle.getEventType():
-            desktop = Desktop.getDesktop()
+            desktop = Desktop.getDesktop()  # noqa: F821 Jython defines Desktop
             desktop.browse(hle.getURL().toURI())
 
