6e7604d907ace8955f265cb8d6ac2b58fcf8bfe3
cak <cak@fastmail.com>
2020-07-18 05:32:11 -0400

    Remove unused variable

--- Remix/BurpRemix/src/main/kotlin/HuntListener.kt
+++ Remix/BurpRemix/src/main/kotlin/HuntListener.kt
@@ -1,7 +1,6 @@
 package burp
 
 class HuntListener(private val callbacks: IBurpExtenderCallbacks, private val huntTab: HuntTab) : IHttpListener {
-    private val helpers: IExtensionHelpers = callbacks.helpers
 
     override fun processHttpMessage(toolFlag: Int, messageIsRequest: Boolean, messageInfo: IHttpRequestResponse?) {
         messageInfo?.let { req ->
